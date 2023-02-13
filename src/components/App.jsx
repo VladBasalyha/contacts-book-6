eb6dc1838c19f3ff6281841de84288617ec00591
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2023-02-13 16:11:54 +0200

    new commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,6 @@ import { Filter } from 'components/FilterField/Filter';
 import { ContactList } from 'components/ContactsList/ContactsList';
 
 export const App = () => {
-  // useEffect(() => {
-  //   // const contacts = localStorage.getItem('contacts');
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <Container>
       <h1>Phonebook</h1>
